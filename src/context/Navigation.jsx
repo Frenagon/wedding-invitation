516c2c046d97a4f3d61b888550e913100a023a38
Francisco Aceves <frenagon@gmail.com>
2022-07-19 01:13:59 -0500

    Added goBack and goNext functions to context

--- src/context/Navigation.jsx
+++ src/context/Navigation.jsx
@@ -11,7 +11,27 @@ const NavigationContext = React.createContext();
 function NavigationProvider({ children }) {
   const [route, setRoute] = React.useState(Routes.HOMEPAGE);
 
-  const value = { route, setRoute };
+  const goBack = React.useCallback(() => {
+    const routeValues = Object.values(Routes);
+    const currentRouteIndex = routeValues.indexOf(route);
+    setRoute(
+      routeValues[
+        currentRouteIndex === 0 ? routeValues.length - 1 : currentRouteIndex - 1
+      ]
+    );
+  }, [route, setRoute]);
+
+  const goNext = React.useCallback(() => {
+    const routeValues = Object.values(Routes);
+    const currentRouteIndex = routeValues.indexOf(route);
+    setRoute(
+      routeValues[
+        currentRouteIndex === routeValues.length - 1 ? 0 : currentRouteIndex + 1
+      ]
+    );
+  }, [route, setRoute]);
+
+  const value = { route, setRoute, goBack, goNext };
   return (
     <NavigationContext.Provider value={value}>
       {children}
