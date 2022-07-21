38bc05ed5d8eeaa888caeabd4353c1bc5a0c988a
Francisco Aceves <frenagon@gmail.com>
2022-07-21 03:09:28 -0500

    Added invitation information

--- src/components/Invitation/Invitation.jsx
+++ src/components/Invitation/Invitation.jsx
@@ -1,12 +1,58 @@
 import WeddingLayout from "../WeddingLayout";
 import NavigationButton from "../NavigationButton";
+import {
+  InvitationContainer,
+  InvitationSection,
+  ParentNames,
+  ParentsBride,
+  ParentsGroom,
+  SpouceNames,
+  LocationSection,
+  FatherName,
+  FooterSection,
+} from "./Invitation.styles";
 
 function Invitation({ closed }) {
   return (
     <WeddingLayout closed={closed}>
       <NavigationButton back />
-      This will be the Invitation
       <NavigationButton />
+      <InvitationContainer>
+        <ParentNames>
+          <ParentsBride>
+            Leopoldo Xavier Cárdenas González
+            <br />
+            Guillermina Sánchez Magaña
+          </ParentsBride>
+          <ParentsGroom>
+            Francisco Javier Aceves Pérez
+            <br />
+            Sofia López Álvarez
+          </ParentsGroom>
+        </ParentNames>
+        <InvitationSection>
+          Tienen el honor de invitarlo
+          <br />
+          al enalce matrimonial de sus hijos:
+        </InvitationSection>
+        <SpouceNames>Myriam y Francisco</SpouceNames>
+        <LocationSection>
+          <span>
+            Que se llevará a cabo el día domingo
+            <br />
+            veintitrés de octubre a la una y media de la tarde,
+            <br />
+            en la cuasiparroquia San Cristóbal Magallanes.
+          </span>
+          <FatherName>
+            Preside el Padre Sergio Arturo Gómez Martínez.
+          </FatherName>
+        </LocationSection>
+        <FooterSection>
+          <span>Se solicita confirmar asistencia.</span>
+          <span>Zapopan, Jalisco, 2022</span>
+        </FooterSection>
+      </InvitationContainer>
     </WeddingLayout>
   );
 }
---
+++ src/components/Invitation/Invitation.styles.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const InvitationContainer = styled.div`
+  height: inherit;
+  width: inherit;
+  margin: 10px;
+  padding: 75px 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: "Handlee", cursive;
+  color: #393939;
+  font-size: 0.75rem;
+  text-align: center;
+`;
+
+export const InvitationSection = styled.div`
+  flex-basis: 20%;
+  display: flex;
+  align-items: center;
+`;
+
+export const ParentNames = styled(InvitationSection)`
+  width: inherit;
+  font-size: 0.688rem;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const ParentsBride = styled.span`
+  text-align: left;
+`;
+
+export const ParentsGroom = styled.span`
+  text-align: right;
+`;
+
+export const SpouceNames = styled(InvitationSection)`
+  font-family: "Ms Madi", cursive;
+  font-size: 2rem;
+  color: #675a34;
+`;
+
+export const LocationSection = styled(InvitationSection)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+export const FatherName = styled.span`
+  font-size: 0.688rem;
+`;
+
+export const FooterSection = styled(InvitationSection)`
+  width: inherit;
+  font-size: 0.688rem;
+  display: flex;
+  justify-content: space-evenly;
+`;
