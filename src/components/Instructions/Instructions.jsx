0fd33ea1e11d21a96695c701315b9165082bf9b0
Francisco Aceves <frenagon@gmail.com>
2022-07-21 00:40:59 -0500

    Added decoration and navigation to invitation and instructions

--- src/components/Instructions/Instructions.jsx
+++ src/components/Instructions/Instructions.jsx
@@ -1,5 +1,18 @@
+import { CornerBorder } from "../../globalStyles";
+import NavigationButton from "../NavigationButton";
+import { InstructionsContainer } from "./Instructions.styles";
+
 function Instructions() {
-  return <div>This will be the Instructions</div>;
+  return (
+    <InstructionsContainer>
+      <CornerBorder top left />
+      <CornerBorder top right rotate="90deg" />
+      <NavigationButton back />
+      This will be the Instructions
+      <CornerBorder bottom left rotate="-90deg" />
+      <CornerBorder bottom right rotate="180deg" />
+    </InstructionsContainer>
+  );
 }
 
 export default Instructions;
--- src/components/Invitation/Invitation.jsx
+++ src/components/Invitation/Invitation.jsx
@@ -1,5 +1,19 @@
+import { CornerBorder } from "../../globalStyles";
+import NavigationButton from "../NavigationButton";
+import { InvitationContainer } from "./Invitation.styles";
+
 function Invitation() {
-  return <div>This will be the Invitation</div>;
+  return (
+    <InvitationContainer>
+      <CornerBorder top left />
+      <CornerBorder top right rotate="90deg" />
+      <NavigationButton back />
+      This will be the Invitation
+      <NavigationButton />
+      <CornerBorder bottom left rotate="-90deg" />
+      <CornerBorder bottom right rotate="180deg" />
+    </InvitationContainer>
+  );
 }
 
 export default Invitation;
